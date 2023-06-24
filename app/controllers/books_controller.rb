1f3989fb1f16d091b793780e479ca9f13cf31811
EC2 Default User <ec2-user@ip-172-31-10-127.ap-northeast-1.compute.internal>
2023-06-24 12:34:35 +0000

    [Update]indｅx.htmlにnewアクションを設置しエラー解消

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,8 +1,4 @@
 class BooksController < ApplicationController
-  def new
-    @book = Book.new
-  end
-
   def create
     @book = Book.new(book_params)
     if @book.save
@@ -14,6 +10,7 @@ class BooksController < ApplicationController
   end
 
   def index
+    @book = Book.new
     @books = Book.all
   end
 
